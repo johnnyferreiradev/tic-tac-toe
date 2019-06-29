4202b724f36713b14f55d796b35138cc730c0e02
johnnyferreiradevweb <johnnyferreiradevweb@gmail.com>
2019-06-29 13:29:07 -0300

    Criaçao da pasta src e model/Ranking e instalaçao do mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>');
-})
+app.use(express.json()) // Permite o recebimento de dados via JSON
+
+// Conexao com o MongoDB
+mongoose.connect('mongodb://localhost:27017/tictactoe', { useNewUrlParser: true })
 
 app.listen(3000, () => {
     console.log('Backend executando na porta 3000...');
---
+++ backend/src/models/Ranking.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+// const mongoosePaginate = require('mongoose-paginate');
+
+const RankingSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    score: {
+        type: Number,
+        required: true
+    },
+    email: {
+        type: String,
+        required: false
+    },
+    password: {
+        type: String,
+        required: false
+    }
+});
+
+mongoose.model('Ranking', RankingSchema);
