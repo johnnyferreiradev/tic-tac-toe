caddb82ea68972d79fe33e5d61357729f3ab7de6
johnnyferreiradevweb <johnnyferreiradevweb@gmail.com>
2019-07-02 10:08:55 -0300

    Criação da Função Store do Controller

--- backend/src/controllers/RankingController.js
+++ backend/src/controllers/RankingController.js
@@ -6,5 +6,23 @@ module.exports = {
         const gamers = await Ranking.find({});
 
         return res.json(gamers);
+    },
+
+    async show(req, res) {
+        // Code ...
+    },
+
+    async store(req, res) {
+        const newPlayer = await Ranking.create(req.body);
+
+        return res.json(newPlayer);
+    },
+
+    async update(req, res) {
+        // Code ...
+    },
+
+    async destroy(req, res) {
+        // Code ...
     }
 }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,6 +4,7 @@ const routes = express.Router();
 const RankingController = require('./controllers/RankingController');
 
 routes.get('/ranking', RankingController.index);
+routes.post('/ranking', RankingController.store);
 
 
 module.exports = routes;
