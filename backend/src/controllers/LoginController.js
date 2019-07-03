26160d7a36735488d44fcc9f2f2ddccd11c78e00
johnnyferreiradevweb <johnnyferreiradevweb@gmail.com>
2019-07-03 11:06:24 -0300

    Criação da autenticação e cadastro via login

--- backend/src/controllers/LoginController.js
+++ backend/src/controllers/LoginController.js
@@ -3,6 +3,21 @@ const Ranking = mongoose.model('Ranking');
 
 module.exports = {
     async login(req, res) {
-        return res.send('Pagina de Login');
+        try {
+            const user = await Ranking.find({ email: req.body.email, password: req.body.password });
+            console.log(user[0]);
+            return res.json({id: user[0]._id, name: user[0].name, score: user[0].score});
+        }catch (e) {
+            return res.status(500).send(`Email ou senha incorretos - Error: ${e}`);
+        }
+    },
+
+    async signup(req, res) {
+        try {
+            const newPlayer = await Ranking.create(req.body);
+            return res.json({id: newPlayer._id, name: newPlayer.name});
+        } catch (e) {
+            return res.status(500).send(`Error: ${e}`);
+        }
     }
 }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,13 +5,12 @@ const RankingController = require('./controllers/RankingController');
 const LoginController = require('./controllers/LoginController');
 
 routes.post('/login', LoginController.login);
+routes.post('/signup', LoginController.signup);
 
 routes.get('/ranking', RankingController.index);
 routes.post('/ranking', RankingController.store);
 routes.get('/ranking/:name', RankingController.show);
 routes.put('/ranking/:id', RankingController.update);
-
-
 
 
 module.exports = routes;
