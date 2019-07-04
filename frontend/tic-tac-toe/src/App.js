dd6128dc2055f7e44a14d0838b0c8fc1815f7896
johnnyferreiradevweb <johnnyferreiradevweb@gmail.com>
2019-07-04 09:58:11 -0300

    Criação do app base e dos compon. header e footer

---
+++ frontend/tic-tac-toe/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/templates/Header/Header';
+import Footer from './components/templates/Footer/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/tic-tac-toe/src/components/templates/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => (
+    <header>
+        <h1>Footer da aplicação!</h1>
+    </header>
+);
+
+export default Footer;
