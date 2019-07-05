09e4685380a17b5e8cff3b0994e4829d53ede269
johnnyferreiradevweb <johnnyferreiradevweb@gmail.com>
2019-07-05 08:37:03 -0300

    Implamentação do menu Home

--- frontend/tic-tac-toe/src/components/pages/Home/Home.jsx
+++ frontend/tic-tac-toe/src/components/pages/Home/Home.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Game from '../Game/Game';
+
+import './Home.css';
 
 const Home = () => (
-    <h1>Home</h1>
+    <section className="home">
+        <Link to="/game_mode1" component={Game} className="button-mode">Um Jogador</Link>
+        <Link to="/game_mode2" component={Game} className="button-mode">Dois Jogadores</Link>
+    </section>
 )
 
 export default Home;
