5a24984e74243adc50079a42864c835a393d1362
johnnyferreiradevweb <johnnyferreiradevweb@gmail.com>
2019-07-26 15:21:55 -0300

    Ajuste de estilo e criacao da render. das jogadas

--- frontend/tic-tac-toe/src/components/pages/Game/Board.jsx
+++ frontend/tic-tac-toe/src/components/pages/Game/Board.jsx
@@ -1,20 +1,56 @@
 import './Board.css';
 
-import React from 'react';
+import React, { Component } from 'react';
 import Field from '../Game/Field';
 
-const Board = () => (
-    <div className="board">
-        <Field className="field northwest-field right-limit bottom-limit"/>
-        <Field className="field north-field bottom-limit"/>
-        <Field className="field northeast-field left-limit bottom-limit"/>
-        <Field className="field west-field right-limit"/>
-        <Field className="field center-field"/>
-        <Field className="field east-field left-limit"/>
-        <Field className="field southwest-field right-limit top-limit"/>
-        <Field className="field south-field top-limit"/>
-        <Field className="field southeast-field left-limit top-limit"/>
-    </div>
-);
+export default class Board extends Component {
 
-export default Board;
+    state = {
+        squares: [
+            '', '', '',
+            '', '', '',
+            '', '', ''
+        ],
+        playValue: 'X'
+    }
+
+    constructor() { // Estratégia utilizada para resolver o problema da referencia do this
+        super()
+        this.toggleValue = this.toggleValue.bind(this)
+    }
+
+    toggleValue() {
+        const playValue = this.state.playValue == 'X' ? 'O' : 'X';
+        this.setState({ playValue })
+    }
+
+    renderSquare(i) {
+        if (this.state.squares[i] !== '') {
+            return
+        }
+
+        let squares = new Array();
+        squares = this.state.squares
+
+        this.toggleValue()
+        squares[i] = this.state.playValue;
+        
+        this.setState({ squares });
+    }
+
+    render() {
+        return (
+            <div className="board">
+                <Field playValue={this.state.squares[0]} onClick={() => this.renderSquare(0)} className="field northwest-field all-limit right-limit bottom-limit" />
+                <Field playValue={this.state.squares[1]} onClick={() => this.renderSquare(1)} className="field north-field left-limit bottom-limit right-limit" />
+                <Field playValue={this.state.squares[2]} onClick={() => this.renderSquare(2)} className="field northeast-field left-limit bottom-limit" />
+                <Field playValue={this.state.squares[3]} onClick={() => this.renderSquare(3)} className="field west-field top-limit right-limit bottom-limit" />
+                <Field playValue={this.state.squares[4]} onClick={() => this.renderSquare(4)} className="field center-field top-limit right-limit bottom-limit left-limit" />
+                <Field playValue={this.state.squares[5]} onClick={() => this.renderSquare(5)} className="field east-field left-limit top-limit bottom-limit" />
+                <Field playValue={this.state.squares[6]} onClick={() => this.renderSquare(6)} className="field southwest-field right-limit top-limit" />
+                <Field playValue={this.state.squares[7]} onClick={() => this.renderSquare(7)} className="field south-field right-limit top-limit left-limit" />
+                <Field playValue={this.state.squares[8]} onClick={() => this.renderSquare(8)} className="field southeast-field left-limit top-limit" />
+            </div>
+        )
+    }
+}
--- frontend/tic-tac-toe/src/components/pages/Game/Field.jsx
+++ frontend/tic-tac-toe/src/components/pages/Game/Field.jsx
@@ -10,8 +10,8 @@ export default class Field extends Component {
 
     render() {
         return (
-            <div className={this.props.className}>
-                X
+            <div className={this.props.className} onClick={this.props.onClick}>
+                <p className="play-value">{this.props.playValue}</p>
             </div>
         )
     }
