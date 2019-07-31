5880533d35d0cd2c03bf364196721ca485201e0a
johnnyferreiradevweb <johnnyferreiradevweb@gmail.com>
2019-07-31 12:25:35 -0300

    Criação do estado do componente Lobby

--- frontend/tic-tac-toe/src/components/pages/Multiplayer/Lobby.jsx
+++ frontend/tic-tac-toe/src/components/pages/Multiplayer/Lobby.jsx
@@ -2,6 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default class Lobby extends React.Component {
+    state = {
+        player1: '',
+        player2: ''
+    }
+
+    updateField(event, player) {
+        if (player === 1) {
+            this.setState({ player1: event.target.value });
+        } else if (player === 2) {
+            this.setState({ player2: event.target.value });
+        }
+    }
+
     render() {
         return (
             <>
@@ -9,19 +22,19 @@ export default class Lobby extends React.Component {
                     <h1>Modo Multiplayer</h1>
 
                     <label htmlFor="gamer1">Jogador 1 (X)</label>
-                    <input type="text" id="gamer1" placeholder="Nome do jogador 1..."/>
+                    <input type="text" id="gamer1" placeholder="Nome do jogador 1..." onChange={e => this.updateField(e, 1)} />
 
                     <label htmlFor="gamer2">Jogador 2 (O)</label>
-                    <input type="text" id="gamer2" placeholder="Nome do jogador 2..."/>
+                    <input type="text" id="gamer2" placeholder="Nome do jogador 2..." onChange={e => this.updateField(e, 2)} />
 
                     {/* Estratégia utilizada para enviar props entre componentes por meio do Link */}
                     <Link to={{ // O state está sendo enviado por meio do Link...
-                            pathname: '/game', // No componente <Game /> as informações passadas estarão disponíveis em this.props.location
-                            state: {
-                                player1: "Johnny",
-                                player2: "John"
-                            }
-                        }} className="btn-start">Jogar!</Link>
+                        pathname: '/game', // No componente <Game /> as informações passadas estarão disponíveis em this.props.location
+                        state: {
+                            player1: this.state.player1,
+                            player2: this.state.player2
+                        }
+                    }} className="btn-start">Jogar!</Link>
                 </div>
             </>
         )
