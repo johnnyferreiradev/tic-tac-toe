ba3d3b09bf5ece3170dcfad9d34dfe14cac09ac1
johnnyferreiradevweb <johnnyferreiradevweb@gmail.com>
2019-09-04 13:55:46 -0300

    implementação dos metodos para adicionar email e senha ao state

--- frontend/tic-tac-toe/src/components/pages/Login/Login.jsx
+++ frontend/tic-tac-toe/src/components/pages/Login/Login.jsx
@@ -1,32 +1,65 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import  './Login.css';
+
+import './Login.css';
 // import '../../../assets/General-styles/styles.css';
 
-const Login = () => (
-    <section className="general-section">
-        <div className="general-box">
-            <h1 className="general-title-form">Bem-Vindo de Volta! :)</h1>
-            <form className="form-general">
+import api from '../../../services/api';
 
-                <label htmlFor="input-email">Email</label>
-                <input type="email" id="input-email" className="general-input-place" placeholder="Digite seu Email..."/>
+export default class Login extends Component {
+    state = {
+        email: '',
+        password: ''
+    }
 
-                <label htmlFor="input-password">Senha</label>
-                <input type="password" id="input-password" className="general-input-place" placeholder="Digite sua senha..."/>
+    renderEmail(event) {
+        this.setState({ email: event.target.value });
+    }
 
-                <button className="general-button-white-to-pink">Entrar</button>
-            
+    renderPassword(event) {
+        this.setState({ password: event.target.value });
+    }
 
-                <p>
-                    Não possui uma conta?      
-                    <Link to="/register" className="button-mode">
-                        <button className="general-button-white-to-pink">Registrar</button>
-                    </Link>
-                </p>
-            </form>
-        </div>
-    </section>
-)
+    // Metodo que verifica se o usuário existe
+    async login() {
+        const response = await api.get(`/ranking/:${this.state.email}`)
+    }
 
-export default Login;
+    render() {
+        return (
+            <section className="general-section">
+                <div className="general-box">
+                    <h1 className="general-title-form">Bem-Vindo de Volta! :)</h1>
+                    <form className="form-general">
+
+                        <label htmlFor="input-email">Email</label>
+                        <input
+                            type="email"
+                            id="input-email"
+                            className="general-input-place"
+                            placeholder="Digite seu Email..."
+                            onChange={(event) => this.renderEmail(event)} />
+
+                        <label htmlFor="input-password">Senha</label>
+                        <input
+                            type="password"
+                            id="input-password"
+                            className="general-input-place"
+                            placeholder="Digite sua senha..."
+                            onChange={(event) => this.renderPassword(event)} />
+
+                        <button className="general-button-white-to-pink">Entrar</button>
+
+
+                        <p>
+                            Não possui uma conta?
+                            <Link to="/register" className="button-mode">
+                                <button className="general-button-white-to-pink">Registrar</button>
+                            </Link>
+                        </p>
+                    </form>
+                </div>
+            </section>
+        )
+    }
+};
