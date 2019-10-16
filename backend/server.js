8148976d845841733863c130752c72a5ddaf89ea
johnnyferreiradevweb <johnnyferreiradevweb@gmail.com>
2019-10-16 10:23:26 -0300

    Ajustes no backend para remoção de avisos

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,11 @@ app.use(express.json()) // Permite o recebimento de dados via JSON
 app.use(cors());
 
 // Conexao com o MongoDB
-mongoose.connect('mongodb://localhost:27017/tictactoe', { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost:27017/tictactoe', { 
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+})
 
 // Faz um require de todos os models
 requireDir('./src/models');
