e9b40490f0a4dcbb42dd8a083e9a9bae7c269a94
johnnyferreiradevweb <johnnyferreiradevweb@gmail.com>
2019-10-16 10:27:24 -0300

    Remoção de função ociosa

--- backend/src/controllers/RankingController.js
+++ backend/src/controllers/RankingController.js
@@ -13,7 +13,6 @@ module.exports = {
     },
 
     async show(req, res) {
-        console.log(req.params);
         try {
             const gamer =  await Ranking.find({ name: req.params.name });
             return res.json({ id: gamer[0]._id, name: gamer[0].name, score: gamer[0].score });
@@ -45,45 +44,7 @@ module.exports = {
                 return res.json({ id: req.params.id, score: currentScore, scoreOfThisMove: req.body.score });
             }
         } catch (e) {
-            console.log('erro no update');
             return res.status(500).send(`Erro no update: ${e}`);
         }
     },
-
-    async calculateScore (req, res) {
-        try {
-            const   win = req.body.win,
-                    plays = req.body.plays,
-                    time = req.body.time,
-                    reset = req.body.reset,
-                    giveUp = req.body.giveUp,
-                    level = req.body.level,
-                    accumScore = req.body.accumScore;
-
-            const WIN_VALUE = 1000000;
-            const PLAY_VALUE = 10;
-            const RESET_VALUE = 200;
-            const GIVEUP_VALUE = 500;
-            
-            if (win != 0 && win != 1) throw "Codigo de Erro 1";
-            if (plays <= 2) throw "Codigo de Erro 2";
-            if (time === 0) throw "Codigo de Erro 3";
-            if (reset < 0) throw "Codigo de Erro 4";
-            if (giveUp !== 0 && giveUp !== 1) throw "Codigo de Erro 5";
-            if (level !== 1 && level !== 2 && level !== 3) throw "Codigo de Erro 6";
-
-            // / ((plays * PLAY_VALUE) * time)) - (reset * RESET_VALUE) - (giveUp * GIVEUP_VALUE);
-            //const score = ((win * WIN_VALUE * level) / (plays * PLAY_VALUE)) / time;
-            //console.log(score);
-            //let scoreNivel = score * (level / 10);
-
-            //scoreNivel = scoreNivel < 0 ? 0 : scoreNivel; 
-
-            //let accumulatedScore = accumScore + scoreNivel;
-            
-            //return res.json({ score: scoreNivel, scoreTotal: accumulatedScore });
-        } catch (e) {
-            return res.send(`Ops! Algo deu errado! Error: ${e}`);
-        }
-    }
 }
--- backend/src/controllers/ScoreController.js
+++ backend/src/controllers/ScoreController.js
@@ -7,7 +7,6 @@ module.exports = {
             const { level, amountOfPlays } = req.body;
             const levelWeight = level * 10000;
             const costPerAttempt = (levelWeight / 100) * amountOfPlays;
-            console.log(costPerAttempt);
             const score = levelWeight - costPerAttempt;
             const intScore = Math.trunc(score)
 
